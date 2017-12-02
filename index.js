1b33eef8452a1d936bba292bfe2767205f38b84d
Kathy Hudson <khitx@icloud.com>
2017-12-02 21:36:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function endGame() {
   }
 */
   var cnt = document.querySelectorAll('.rock').length
-
+//spies.push(expect.spyOn(rock, 'remove'))
   for (var i = 0; i < cnt; i++) {
     document.querySelector('.rock').remove()
   }
